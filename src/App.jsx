4abbc3c7eb43862fef9bc86e36a51a7863766b2c
vikash <vikash.tomar.india@gmail.com>
2023-08-23 23:16:05 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+ import axios from 'axios'
+import React, { useState } from 'react'
+
+ 
+ function App() {
+  const[movie,setMovie] = useState("")
+
+
+  function handlesubmit(e){
+    e.preventDefault()
+
+    axios.get(`https://api.themoviedb.org/3/search/movie?query=${movie}api_key=ea85630803e293cff396198c4410a67b`)
+
+    .then((res)=>
+    {
+      setMovie(res.data.results)
+      console.log(res.data.results[0])
+    })
+  }
+   return (
+     <div>
+
+      <form onSubmit={handlesubmit}>
+        <input type="text" placeholder='Search Movie'  />
+        <button type='submit'>Search</button>
+      </form>
+
+     </div>
+   )
+ }
+ 
+ export default App
