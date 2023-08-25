c6706f842d7ac931f8c6ca831ed4fa4f0e3ccb76
vikash <vikash.tomar.india@gmail.com>
2023-08-25 15:02:09 +0530

    working now

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
- import React, { useState } from 'react'
- import axios from 'axios';
+ import axios from 'axios'
+import React, { useState } from 'react'
  
  function App() {
-
-  const [movie,setMovie] = useState([])
-  const [movieName,setMovieName] = useState('')
-
-  function handlesubmit(){
-    axios.get(`https://api.themoviedb.org/3/search/movie?query=${movie}api_key=ea85630803e293cff396198c4410a67b`)
+  const [movieName,setMovieName] = useState('');
+  const [movie,setMovie] = useState([]);
+let imgpath = "https://image.tmdb.org/t/p/original"
+  function handleSubmit(e)
+  {
+    e.preventDefault()
+    axios.get("https://api.themoviedb.org/3/search/movie?query="+ movieName +"&api_key=ea85630803e293cff396198c4410a67b")
     .then((res)=>{
-      setMovie(res.data.results[0])
-      console.log(res.data.results)
+      setMovie(res.data.results)
+      console.log(res)
+      
     })
   }
 
+  // handleSubmit()
   return (
-    <div>
-        <form action={handlesubmit}>
-          <input type="text" placeholder='Search Movie' value={movieName} onChange={e=> setMovieName (e.target.value)}/>
-          <button type='submit'>Search</button>
-        </form>
+     <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Search Movie Name'  value={movieName}  onChange={(e)=> setMovieName(e.target.value)}/>
+        <button type='submit'>Search</button>
+      </form>
+
+      {movie.map((val,ind)=>{
+        return(
+        <img src={imgpath + movie[0].poster_path } alt="hi" />
+        )
+      })}
 
      </div>
    )
