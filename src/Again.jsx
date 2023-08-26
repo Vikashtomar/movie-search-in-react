4265537cf0d6df9acba1c1f9b4e69c92f2427a94
vikash <vikash.tomar.india@gmail.com>
2023-08-26 23:11:45 +0530

    data checking

--- src/Again.jsx
+++ src/Again.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 function Again() {
     const [data,setData] = useState([])
     const [movie,setMovie] = useState('')
+    let imgpath = "https://image.tmdb.org/t/p/original"
     function handlesubmit(e){
         e.preventDefault()
         axios.get("https://api.themoviedb.org/3/search/movie?query="+ movie +"&api_key=ea85630803e293cff396198c4410a67b")
@@ -12,10 +13,21 @@ function Again() {
         })
 
     }
-    handlesubmit()
+    
   return (
     <div>
+<form onChange={handlesubmit}>
+  <input type="text" placeholder='Search for movie' value={movie} onChange={(e)=> setMovie(e.target.value)} />
+  <button type='submit'>Search </button>
+</form>
 
+{data.map((value,index)=>{
+  return (
+    <div>
+    <img  src={imgpath + data[index].poster_path} alt="hi" />
+    </div>
+  )
+})}
 
     </div>
   )
